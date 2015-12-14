c57b4eeb032a2c0777886831c49d3bbacea68dae
Shown Tien <hightian@gmail.com>
2015-12-14 14:59:00 +0800

    update __init__ of api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,11 @@
 # coding: utf8
 
 from flask import Blueprint
-from config import environment
 from flask.ext.cors import CORS
 
 api = Blueprint('api', __name__)
 CORS(api)
 
 from app.api import error
+
+__all__ = [error]
